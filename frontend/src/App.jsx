c5c8978d6f53fda048725e75a423e8bd07cb2785
decaylee13 <151460398+decaylee13@users.noreply.github.com>
2025-11-08 23:19:06 -0500

    Add skeleton anatomy experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './Home.jsx';
 import HeartExperience from './HeartExperience.jsx';
 import BrainExperience from './BrainExperience.jsx';
+import SkeletonExperience from './SkeletonExperience.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/heart" element={<HeartExperience />} />
       <Route path="/brain" element={<BrainExperience />} />
+      <Route path="/skeleton" element={<SkeletonExperience />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
